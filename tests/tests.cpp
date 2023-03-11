78620433de2d3592902a079a56fa79f83f8fa956
Artii4 <noemail@used.com>
2023-03-11 19:11:35 +0100

    Added basix lexer tests

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -1,8 +1,66 @@
 #include "../src/headers.h"
 #include <cassert>
 #include <iostream>
+#include <vector>
+
+using namespace std;
+
+bool lexer_throws_exception_on_input(string input) {
+  try {
+    Lexer l(input);
+    while (true) {
+      Token current = l.next_token();
+      if (current.get_type() == END)
+        break;
+    }
+  } catch (...) {
+    return true;
+  }
+
+  return false;
+}
+
+bool lexer_matches_expected_types(string input, vector<Token_type> expected) {
+	Lexer l(input);
+	vector<Token_type> received;
+
+	while (true) {
+		Token t = l.next_token();
+		received.push_back(t.get_type());
+
+		if (t.get_type() == END) break;
+	}
+
+	return received == expected;
+}
+
+void test_lexer() {
+  assert(lexer_throws_exception_on_input("="));
+  assert(lexer_throws_exception_on_input("13.1"));
+
+  assert(!lexer_throws_exception_on_input("(13)"));
+  assert(!lexer_throws_exception_on_input("john"));
+  assert(!lexer_throws_exception_on_input("print 3 uit;"));
+  assert(!lexer_throws_exception_on_input(""));
+  assert(!lexer_throws_exception_on_input("1 + 1"));
+  assert(!lexer_throws_exception_on_input("1 - 1"));
+  assert(!lexer_throws_exception_on_input("1 * 1"));
+  assert(!lexer_throws_exception_on_input("1 / 1"));
+  assert(!lexer_throws_exception_on_input("\r\n\t "));
+
+	assert(lexer_matches_expected_types("", {END}));
+	assert(lexer_matches_expected_types("13", {NUMBER, END}));
+	assert(lexer_matches_expected_types("13+2", {NUMBER, PLUS, NUMBER, END}));
+	assert(lexer_matches_expected_types("print;", {PRINT, SEMICOLON, END}));
+	assert(lexer_matches_expected_types("print pi uit;", {PRINT, NAME, UIT, SEMICOLON, END}));
+	assert(lexer_matches_expected_types("laat x 15 zijn;", {LAAT, NAME, NUMBER, ZIJN, SEMICOLON, END}));
+	assert(lexer_matches_expected_types("laat pi 3 zijn en print het uit;", {LAAT, NAME, NUMBER, ZIJN, EN, PRINT, HET, UIT, SEMICOLON, END}));
+}
+
+void test_all() { test_lexer(); }
 
 int main() {
-	std::cout << "all tests passed :) (LESGOOOO!!!)" << std::endl;
-	return 0;
+  test_all();
+  cout << "all tests passed :) (LESGOOOO!!!)" << endl;
+  return 0;
 }
